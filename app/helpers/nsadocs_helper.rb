b3ac84699461244cecdcc90d4782cabbdc576073
M. C. McGrath <shidash@shidash.com>
2014-09-08 08:13:46 -0700

    Refactored facet link generator, fixed params bug

--- app/helpers/nsadocs_helper.rb
+++ app/helpers/nsadocs_helper.rb
@@ -6,75 +6,89 @@ module NsadocsHelper
     "records_collected" => "Records Collected", "legal_authority" => "Legal Authority", "countries" => "Countries",
     "sigads" => "SIGADS", "released_by" => "Released By"}
     
+    # Go through all facets
     facetNames.each do |f|
       outhtml += genFacet(facets, "", f) if facets[f[0]]["terms"].count > 0
     end
     return outhtml
   end
 
+  # Gen html for list of links for each facet
   def genFacet(facets, outhtml, type)
     outhtml += '<ul class="nav nav-list">
             <li><label class="tree-toggler nav-header just-minus">'+type[1]+'</label><ul class="nav nav-list tree">'
-
+    facetname = type[0]+"_facet"
+    facetval = params[facetname]
+    
     # Overflow settings
     totalnum = facets[type[0]]["terms"].count
     numshow = totalnum > 5 ? 5+totalnum*0.01 : totalnum
     overflow = '<li><label class="tree-toggler nav-header plus"></label><ul class="nav nav-list tree collapse">'
     counter = 0
 
-    
-    # Add each facet to output or overflow
+    # Add each facet to output or overflow list
     facets[type[0]]["terms"].each do |i|
       if counter > numshow
-        if params[type[0]+"_facet"] == i["term"] || (params[type[0]+"_facet"].is_a?(Array) && params[type[0]+"_facet"].include?(i["term"]))
-          # Direct to root path if there's no other query or filters
-          if params.except("controller", "action", "utf8", type[0]+"_facet").length > 0 || params[type[0]+"_facet"].is_a?(Array)
-            overflow += "<li><em>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.except("utf8", type[0]+"_facet")))+"</em></li>"
-          else
-            overflow += "<li><em>"+link_to("#{i["term"]} (#{i["count"].to_s})", root_path)+"</li></em>"
-          end
-        else
-          # Normal links
-          if params[type[0]+"_facet"]
-            facetvals = params[type[0]+"_facet"].is_a?(Array) ? params[type[0]+"_facet"].push(i["term"]) : [params[type[0]+"_facet"], i["term"]]
-            overflow += "<li>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.merge("utf8" => "✓", type[0]+"_facet" => facetvals)))+"</li>"
-          else
-            overflow += "<li>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.merge("utf8" => "✓", type[0]+"_facet" => i["term"])))+"</li>"
-          end
-      end
+        overflow += termLink(i, facetval, facetname)
       else
-        # Show normal or highlighted link
-        if params[type[0]+"_facet"] == i["term"] || (params[type[0]+"_facet"].is_a?(Array) && params[type[0]+"_facet"].include?(i["term"]))
-          # Direct to root path if there's no other query or filters
-          if params.except("controller", "action", "utf8", type[0]+"_facet").length > 0 || params[type[0]+"_facet"].is_a?(Array)
-            # Handle if it is an array or not
-            if params[type[0]+"_facet"].is_a?(Array)
-              outval = params[type[0]+"_facet"].count #write this
-              outhtml += "<li><em>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.except(type[0]+"_facet").merge(type[0]+"_facet" => outval)))+"</em></li>"
-              # Test one case
-              # Test two cases
-              # Test 3 cases
-              # Add to overflow
-            else
-              outhtml += "<li><em>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.except(type[0]+"_facet")))+"</em></li>"
-            end
-          else
-            outhtml += "<li><em>"+link_to("#{i["term"]} (#{i["count"].to_s})", root_path)+"</li></em>"
-          end
-        else
-          if params[type[0]+"_facet"]
-            facetvals = params[type[0]+"_facet"].is_a?(Array) ? params[type[0]+"_facet"].push(i["term"]) : [params[type[0]+"_facet"], i["term"]]
-            outhtml += "<li>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.merge("utf8" => "✓", type[0]+"_facet" => facetvals)))+"</li>"
-          else
-            outhtml += "<li>"+link_to("#{i["term"]} (#{i["count"].to_s})", search_path(params.merge("utf8" => "✓", type[0]+"_facet" => i["term"])))+"</li>"
-          end
-        end 
+        outhtml += termLink(i, facetval, facetname)
       end
       counter += 1
     end
+
     overflow += "</li></ul>"
     outhtml += "#{overflow}" if counter > numshow
     outhtml += "</ul></li></ul><br />"
     return outhtml
+  end
+
+  # Generate link html for each term in facet list
+  def termLink(i, facetval, facetname)
+    linkname = "#{i["term"]} (#{i["count"].to_s})"
+
+    # Check if link is selected or not
+    if facetval == i["term"] || (facetval.is_a?(Array) && facetval.include?(i["term"]))
+      return selected(i, facetval, facetname, linkname)
+    else
+      return notSelected(i, facetval, facetname, linkname)
+    end
+  end
+
+  # Generate link for selected facet
+  def selected(i, facetval, facetname, linkname)
+    # Check if there are other facets selected (in this category or others)
+    if params.except("controller", "action", "utf8", facetname).length > 0 || facetval.is_a?(Array)
+      # Are there other facets selected in this category?                   
+      if facetval.is_a?(Array)
+        outval = facetval.dup
+        outval.delete(i["term"])
+        outval = outval[0] if facetval.count <= 2
+        return genLink(linkname, search_path(params.except(facetname).merge(facetname => outval)), true)
+      else # If no others in category selected
+        return genLink(linkname, search_path(params.except(facetname)), true)
+      end
+    else # If no others selected
+      return genLink(linkname, root_path, true)
+    end
+  end
+
+  # If facet is not selected, generate link
+  def notSelected(i, facetval, facetname, linkname)
+    retstr = ""
+    if facetval # Check if another facet in the same category is selected
+      facetvals = facetval.is_a?(Array) ? facetval.dup.push(i["term"]) : [facetval, i["term"]]
+      retstr = genLink(linkname, search_path(params.merge("utf8" => "✓", facetname => facetvals)), false)
+      params.except(facetname).merge(facetname => facetval)
+    else
+      retstr = genLink(linkname, search_path(params.merge("utf8" => "✓", facetname => i["term"])), false)
+      params.except(facetname)
+    end
+
+    return retstr
+  end
+
+  # Generate link html
+  def genLink(name, path, em)
+    return (em ? "<li><em>" : "<li>") + link_to(name, path) + (em ? "</li></em>" : "</li>")
   end
 end
