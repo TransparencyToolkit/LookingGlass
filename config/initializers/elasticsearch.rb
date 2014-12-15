013d07efb04f5df801bda5e7710fcd753e1d2dc9
Debian Live user <shidash@shidash.com>
2014-12-15 04:42:12 +0000

    Changed from localhost to 127.0.0.1

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_URL'] || 'localhost:9200')
+Elasticsearch::Persistence.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_URL'] || '127.0.0.1:9200')
 
 if Rails.env.development?
   logger           = ActiveSupport::Logger.new(STDERR)
