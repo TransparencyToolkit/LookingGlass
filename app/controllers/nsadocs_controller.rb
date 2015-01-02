1a0adc02f4991eaeb81531ab945dfd72e4c9bc6f
Debian Live user <shidash@shidash.com>
2015-01-02 04:25:30 +0000

    Fixed typo in length field name on table list

--- app/controllers/nsadocs_controller.rb
+++ app/controllers/nsadocs_controller.rb
@@ -3,16 +3,25 @@ class NsadocsController < ApplicationController
 
   def index
     @nsadocs = Nsadoc.all
-    results = Nsadoc.search facets: {
-      programs: {terms: {field: "programs", size: 1000}},
-      codewords: {terms: {field: "codewords", size: 1000}},
-      type: {terms: {field: "type", size: 1000}},
-      records_collected: {terms: {field: "records_collected", size: 1000}},
-      legal_authority: {terms: {field: "legal_authority", size: 1000}},
-      countries: {terms: {field: "countries", size: 1000}},
-      sigads: {terms: {field: "sigads", size: 1000}},
-      released_by: {terms: {field: "released_by", size: 1000}}
-    }
+    fieldList= JSON.parse(File.read("app/dataspec/nsadata.json"))
+    fieldhash = Array.new
+    fieldList.each do |f|
+      if f["Searchable?"] == "Yes"
+        fieldhash << f["Field Name"].to_sym} => {:terms => {:field => {f["Field Name"]}, :size => {f["Size"]}}}
+      end
+    end
+    binding.pry
+    results = Nsadoc.search fieldhash
+    #facets: {
+     # programs: {terms: {field: "programs", size: 1000}},
+      #codewords: {terms: {field: "codewords", size: 1000}},
+      #type: {terms: {field: "type", size: 1000}},
+      #records_collected: {terms: {field: "records_collected", size: 1000}},
+      #legal_authority: {terms: {field: "legal_authority", size: 1000}},
+      #countries: {terms: {field: "countries", size: 1000}},
+      #sigads: {terms: {field: "sigads", size: 1000}},
+      #released_by: {terms: {field: "released_by", size: 1000}}
+    #}
     @facets = results.response["facets"]
   end
 
