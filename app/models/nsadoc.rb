0520c40aca5eab700c62227fc2f725f00b11e259
Debian Live user <shidash@shidash.com>
2015-01-04 19:08:30 +0000

    Dynamic generation of mapping from dataspec

--- app/models/nsadoc.rb
+++ app/models/nsadoc.rb
@@ -1,27 +1,23 @@
 class Nsadoc
   include Elasticsearch::Persistence::Model
 
-  attribute :release_date, Date, mapping: {index: 'not_analyzed'}
-  attribute :released_by, String, mapping: {index: 'not_analyzed'}
-  attribute :released_by_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :article_url, String, mapping: {index: 'not_analyzed'}
-  attribute :title, String, mapping: {analyzer: 'english'}
-  attribute :doc_path, String, mapping: {index: 'not_analyzed'}
-  attribute :type, String, mapping: {index: 'not_analyzed'}
-  attribute :type_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :legal_authority, String, mapping: {index: 'not_analyzed'}
-  attribute :legal_authority_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :records_collected, String, mapping: {index: 'not_analyzed'}
-  attribute :records_collected_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :creation_date, Date, mapping: {index: 'not_analyzed'}
-  attribute :doc_text, String, mapping: {analyzer: 'english'}
-  attribute :aclu_desc, String, mapping: {analyzer: 'english'}
-  attribute :sigads, String, mapping: {index: 'not_analyzed'}
-  attribute :sigads_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :codewords, String, mapping: {index: 'not_analyzed'}
-  attribute :codewords_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :programs, String, mapping: {index: 'not_analyzed'}
-  attribute :programs_analyzed, String, mapping: {analyzer: 'english'}
-  attribute :countries, String, mapping: {index: 'not_analyzed'}
-  attribute :countries_analyzed, String, mapping: {analyzer: 'english'}
+  fieldList = JSON.parse(File.read("app/dataspec/nsadata.json"))
+  fieldhash = Hash.new
+  fieldList.each do |f|
+    # Set mapping
+    map = Hash.new
+    if f["Mapping"] == "not_analyzed"
+      map[:index] = f["Mapping"]
+    elsif f["Mapping"] == "english"
+      map[:analyzer] == f["Mapping"]
+    end
+    
+    # If facet, make separate analyzed version. 
+    if f["Facet?"] == "Yes"
+      attribute (f["Field Name"]+"_analyzed").to_sym, f["Type"], mapping: map
+      attribute f["Field Name"].to_sym, f["Type"], mapping: {index: "not_analyzed"}
+    else # For non-facets                    
+      attribute f["Field Name"].to_sym, f["Type"], mapping: map
+    end
+  end
 end
