2ea4452b1f2fc252f7f96cc06e7a32ac364f1229
GH <shidash@shidash.com>
2015-02-26 13:57:59 +0100

    Added import from directories (with tagging based on file name and dir names)

--- lib/index_manager.rb
+++ lib/index_manager.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'pry'
 
 class IndexManager
   def self.create_index(options={})
@@ -20,12 +21,26 @@ class IndexManager
     create_index force: true if options[:force]
 
     import_info = JSON.parse(File.read("app/dataspec/importer.json")).first
-
+    doc = Array.new
+    
     # Import files, files from links, and directories of files
     if import_info["Path Type"] == "File"
       doc = JSON.parse(File.read(import_info["Path"]), symbolize_names: true)
     elsif import_info["Path Type"] == "url"
       doc = JSON.parse(URI.parse(import_info["Path"]).read, symbolize_names: true)
+    elsif import_info["Path Type"] == "Directory"
+      Dir.glob(import_info["Path"]+"/**/*.json") do |file|
+        if !file.include? import_info["Ignore Dir Import Ext"]
+          dataset_name = file.split("/").last.gsub("_", " ").gsub(".json", "")
+          categories = file.gsub(import_info["Path"], "").gsub(file.split("/").last, "").split("/").reject(&:empty?)
+          
+          # Add the dataset_name and categories to each item
+          file_items = JSON.parse(File.read(file))
+          file_items.each do |i|
+            doc.push(i.merge(dataset_name: dataset_name, categories: categories))
+          end
+        end
+      end
     end
     
     nsadocs = doc.map { |nsadoc_hash| process_nsadoc_info(nsadoc_hash, import_info)}
