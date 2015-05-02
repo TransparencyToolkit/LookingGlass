f090b3d24b60cd429ced24dab6953456a15bf0f3
Brennan Novak <hi@brennannovak.com>
2015-05-02 18:35:53 +0200

    better handling of empty or null values in linkedFacets() helper

--- app/helpers/table_format.rb
+++ app/helpers/table_format.rb
@@ -13,18 +13,21 @@ module TableFormat
     end
   end
 
-  # Generates links to data filtered by facet val   
+  # Generates links to data filtered by facet val
   def linkedFacets(field_vals, field_name)
     outstr = ""
 
+    # Generate links for each value in list of facet vals
     if field_vals.is_a?(Array)
-      # Generate links for each value in list of facet vals                                        
       field_vals.each do |i|
         outstr += link_to(i, search_path((field_name+"_facet").to_sym => i))
         outstr += ", " if i != field_vals.last
       end
-    else # For single values
-      outstr += link_to(field_vals, search_path((field_name+"_facet").to_sym => field_vals))
+    # For single values
+    else
+      if field_vals 
+        outstr += link_to(field_vals, search_path((field_name+"_facet").to_sym => field_vals))
+      end
     end
     return outstr
   end
