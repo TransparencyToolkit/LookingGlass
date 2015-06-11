af40f704db39a4472309722a3ff7514a2dc6e74a
Brennan Novak <hi@brennannovak.com>
2015-06-11 12:04:03 +0200

    added show article in a Tab

--- app/assets/javascripts/looking-glass.js
+++ app/assets/javascripts/looking-glass.js
@@ -51,4 +51,26 @@ $(document).ready(function() {
       .show();
   });
 
+
+  // Show Article in Tab
+  $('.show-article-tab').on('click', function(e) {
+
+    e.preventDefault();
+
+    $(this).attr('href');
+    $(this).data('index');
+    var new_tab_id = 'article-' + $(this).data('index');
+
+    $('#maincontent').find('.nav-pills').append('<li id="li' + new_tab_id + '">\
+      <a href="#' + new_tab_id + '" data-toggle="tab"> Article #' + $(this).data('index') + '</a>\
+    </li>');
+
+    $('#maincontent').find('.tab-content').append('<div class="tab-pane" id="' + new_tab_id + '">\
+      <iframe class="show-iframe" src="' + $(this).attr('href') + '" width="100%" height="700" sandbox="allow-same-origin"></iframe>\
+    </div>');
+
+    $('#li' + new_tab_id + ' a[href="#' + new_tab_id + '"]').tab('show');
+  });
+
+
 });
