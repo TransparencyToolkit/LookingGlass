15cdb3f3c10c50db36cdc97a45bd756cf5d1dc91
GH <shidash@shidash.com>
2015-08-22 04:00:08 +0200

    Refactored doc format helper

--- app/helpers/doc_format.rb
+++ app/helpers/doc_format.rb
@@ -16,25 +16,58 @@ module DocFormat
     return link_to this_url, this_url, target: "_blank"
   end
 
+  # Prepare a picture field
+  def preparePicture(doc, field)
+    return image_tag(doc[field["Field Name"]], :class => "picture")
+  end
+
+  # Format field names
+  def prepareFieldName(field)
+    return '<strong>'+field["Human Readable Name"]+': </strong>'
+  end
+
+  # Format longer text fields
+  def prepareLongText(doc, field)
+    return raw(format_text(doc[field["Field Name"]])) if doc[field["Field Name"]]
+  end
+
+  # Handles both single links and arrays of links
+  def handleLinkFields(doc, field)
+    # Get link field content
+    links = doc[field["Field Name"]]
+    outstr = ''
+
+    # Handle multiple links
+    if links.is_a?(Array)
+      links.each do |u|
+        outstr = outstr + prepareLink(u.to_s) + '<br />'
+      end
+    else # Handle single links
+      outstr += prepareLink(links.to_s)
+    end
+
+    return outstr
+  end
+
   # Wraps field name in tags and gets doc text for showing in doc   
   def prepareField(field, doc)
     outstr = ''
+
+    # Handle picture fields
     if field["Display Type"] == "Picture"
-      outstr += image_tag(doc[field["Field Name"]], :class => "picture")
-    else
-      outstr += '<strong>'+field["Human Readable Name"]+': </strong>'
+      outstr += preparePicture(doc, field)
+    else # Handle fields that need a field name first
+      outstr += prepareFieldName(field)
+      
+      # Long text
       if checkIfX(field, @truncated_fields)
-        outstr += raw format_text(doc[field["Field Name"]]) if doc[field["Field Name"]]
+        outstr += prepareLongText(doc, field)
+
+      # Links
       elsif field["Display Type"] == "Link"
-        # Handle single and arrays of links
-        if doc[field["Field Name"]].is_a?(Array)
-          doc[field["Field Name"]].each do |u|
-            outstr = outstr + prepareLink(u.to_s) + '<br />'
-          end
-        else
-          outstr += prepareLink(doc[field["Field Name"]].to_s)
-        end
-        return outstr
+        outstr += handleLinkFields(doc, field)
+
+      # Any other text
       else
         outstr += doc[field["Field Name"]].to_s
       end
