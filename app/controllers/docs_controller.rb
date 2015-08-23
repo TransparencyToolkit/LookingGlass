f8cde7e2a218fe57bc64e1acd81f52aa2014b8cf
GH <shidash@shidash.com>
2015-08-23 04:02:29 +0200

    Refactored docs controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -14,14 +14,10 @@ class DocsController < ApplicationController
 
     pagenum = params[:page] ? params[:page].to_i : 1
     start = pagenum*30-30
-
-    # Sort if necessary
-    if !@sort_field.empty?
-      @docs = Doc.search(sort: {@sort_field => "desc"}, from: start, size: 30, facets: fieldhash)
-    else
-      @docs = Doc.search(from: start, size: 30, facets: fieldhash)
-    end
     
+    sort_results(start, fieldhash)
+
+    # Get all facets and documents
     @pagination = WillPaginate::Collection.create(pagenum, 30, Doc.count) do |pager|
       pager.replace @docs
     end
@@ -34,19 +30,8 @@ class DocsController < ApplicationController
     @docs = ""
     @link_type = Hash.new
 
-    @field_info.each do |f|
-      if f["Link"]
-        @link_type["Link Field"] = f["Field Name"]
-        @link_type["Link Type"] = f["Link"]
-      end
-    end
-
-    # Match items by link field
-    if @link_type["Link Type"] == "mult_items"
-      @docs = Doc.search(query: { match: {@link_type["Link Field"].to_sym => Doc.find(params[:id])[@link_type["Link Field"]] }}, size: 999999)
-    else
-      @doc = Doc.find(params[:id])
-    end
+    set_link_field
+    get_matching_set
     
     respond_to do |format|
       format.html
@@ -57,5 +42,33 @@ class DocsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_doc
         @doc = Doc.find(params[:id])
+    end
+
+    # Sorts the results
+    def sort_results(start, fieldhash)
+      if !@sort_field.empty?
+        @docs = Doc.search(sort: {@sort_field => "desc"}, from: start, size: 30, facets: fieldhash)
+      else
+        @docs = Doc.search(from: start, size: 30, facets: fieldhash)
+      end
+    end
+
+    # Set field that links items
+    def set_link_field
+      @field_info.each do |f|
+        if f["Link"]
+          @link_type["Link Field"] = f["Field Name"]
+          @link_type["Link Type"] = f["Link"]
+        end
+      end
+    end
+
+    # Get all items with matching link field
+    def get_matching_set
+      if @link_type["Link Type"] == "mult_items"
+        @docs = Doc.search(query: { match: {@link_type["Link Field"].to_sym => Doc.find(params[:id])[@link_type["Link Field"]] }}, size: 999999)
+      else
+        @doc = Doc.find(params[:id])
+      end
     end
 end
