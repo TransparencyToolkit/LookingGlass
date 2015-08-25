0958d951a1c1b681e4f64041033a60f823079b3e
GH <shidash@shidash.com>
2015-08-25 04:48:26 +0200

    Refactored import methods

--- lib/import_support.rb
+++ lib/import_support.rb
@@ -9,19 +9,30 @@ module ImportSupport
     createFromFile(JSON.parse(File.read(@data_path), symbolize_names: true))
   end
 
-  # Handles the processing of each item in a file in a directory import
-  def importFileInDir(file)
-    # Get dataset name (file name) and categories (directory names)
-    dataset_name = file.split("/").last.gsub("_", " ").gsub(".json", "")
+  
+  # Get dataset name (file name) - for import from dir
+  def getDatasetName(file)
+    return file.split("/").last.gsub("_", " ").gsub(".json", "")
+  end
+
+  # Get categories (dir name) - for import from dir
+  def getNameCategories(file, dataset_name)
     categories = file.gsub(@data_path, "").gsub(file.split("/").last, "").split("/").reject(&:empty?)
     categories.push(dataset_name)
+    return categories
+  end
 
-    # Add the dataset_name and categories to each item, do any extra processing, then create
-    count = 0
-    # Handle null values instead of JSON
+  # Open file for dir import (if not null)
+  def openFile(file)
     file_text = File.read(file)
-    file_items = JSON.parse(file_text) if file_text != "null"
+    return JSON.parse(file_text) if file_text != "null"
+  end
 
+  # Append categories to file items and create
+  def appendCategories(file_items, dataset_name, categories)
+    count = 0
+
+    # Loop through all items if not nill or empty
     if file_items != nil && !file_items.empty?
       file_items.each do |i|
         i.merge!(dataset_name: dataset_name, categories: categories)
@@ -29,5 +40,15 @@ module ImportSupport
         count += 1
       end
     end
+  end
+  
+  # Handles the processing of each item in a file in a directory import
+  def importFileInDir(file)
+    # Get dataset name and categories
+    dataset_name = getDatasetName(file)
+    categories = getNameCategories(file, dataset_name)
+
+    # Append categories to items and create
+    appendCategories(openFile(file), dataset_name, categories)    
   end
 end
