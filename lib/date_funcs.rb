8850993ca37ae2ec9f4f8b1b3ba2fdaeacf8e2f5
GH <shidash@shidash.com>
2015-08-25 04:27:52 +0200

    Refactored date functions

--- lib/date_funcs.rb
+++ lib/date_funcs.rb
@@ -12,33 +12,80 @@ module DateFuncs
     return false
   end
 
+  # Set date field name as symbol or string as needed
+  def set_name(f, item)
+    date_field = f["Field Name"].to_sym
+    date_field = item[date_field] == nil ? date_field.to_s : date_field
+    return date_field
+  end
+
+  # Blanks the date field if it matches specified terms
+  def blank_if_match(f, item, date_field, terms)
+    terms.each do |t|
+      item[date_field] = "" if item[date_field].to_s.include?(t)
+    end
+
+    return item
+  end
+
+  # Normalize unknown dates
+  def handle_unknown_dates(f, item, date_field)
+    # List of unknown date options
+    unknown = ["Date unknown", "Unknown", "nodate", "0000-00-00 00:00:00"]
+
+    # Check against unknown options
+    item = blank_if_match(f, item, date_field, unknown)
+    
+    # Handle blank dates
+    item[date_field] = "" if item[date_field].empty?
+    return item
+  end
+
+  # Handle present dates
+  def handle_present_dates(f, item, date_field)
+    # List of words for present
+    present = ["Present", "Current", "Gegenwart"]
+
+    # Blank present dates
+    return blank_if_match(f, item, date_field, present)
+  end
+
+
+  # Handle year only dates
+  def handle_year_only(f, item, date_field)
+    if item[date_field].to_s.length == 4 || item[date_field].to_s.length == 5
+      item[date_field] = Date.parse("January " + item[date_field].to_s)
+    end
+
+    return item
+  end
+  
+  # Handle foreign dates
+  def handle_foreign_dates(f, item, date_field)
+    if !item[date_field].to_s.empty?
+      former_date = item[date_field]
+      item[date_field] = Date.parse(normalize_date(item[date_field]).to_s)
+    end
+
+    return item
+  end
+
   # Processes dates and handles unknowns
   def process_date(f, item)
     if f["Type"] == "Date"
       # Set date field to symbol or string as needed
-      date_field = f["Field Name"].to_sym
-      date_field = item[date_field] == nil ? date_field.to_s : date_field
+      date_field = set_name(f, item)
+      
+      # Handle unknown and present dates
+      handle_unknown_dates(f, item, date_field)
+      handle_present_dates(f, item, date_field)
 
-      # Normalize unknown vals
-      if item[date_field] == "Date unknown" || item[date_field] == "Unknown" || item[date_field] == "nodate" || item[date_field].to_s == "0000-00-00 00:00:00" || item[date_field].empty?
-        item[date_field] = ""
-
-        # Normalize present vals for end dates
-      elsif item[date_field].include?("Present") || item[date_field].include?("Current") || item[date_field].include?("Gegenwart")
-        item[date_field] = ""
-      end
-
-      # Handle dates that aren't parsed correctly
+      # Handle dates that are parsed correctly
       begin
         item[date_field] = Date.parse(item[date_field])
-      rescue
-        # Handle year only dates
-        if item[date_field].to_s.length == 4 || item[date_field].to_s.length == 5
-          item[date_field] = Date.parse("January " + item[date_field].to_s)
-        elsif !item[date_field].empty? # Handle foreign dates
-          former_date = item[date_field]
-          item[date_field] = Date.parse(normalize_date(item[date_field]).to_s)
-        end
+      rescue # Handle dates that aren't parsed correctly
+        item = handle_year_only(f, item, date_field)
+        item = handle_foreign_dates(f, item, date_field)
       end
     end
     return item
@@ -58,7 +105,7 @@ module DateFuncs
         row.each_with_index do |item, i_index|
 
           # Check all other language row items to see if it is included
-          if i_index != 0 && date.include?(item)
+          if i_index != 0 && date.to_s.include?(item)
             return date.sub(item, row[0])
           end
 
