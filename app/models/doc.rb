3239e7af82c3c00125ac8063b77bcdd811594d07
GH <shidash@shidash.com>
2015-09-04 16:51:25 +0200

    Refactored mapping

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,35 +1,16 @@
+load 'model_gen.rb'
+
 class Doc
   extend IndexMethods
   include Elasticsearch::Persistence::Model
   include ENAnalyzer
+  extend ModelGen
 
   # Load dataspec, analyzer settings and set index name
   loadDataspec
-
   index_name @index_name
+
+  # Get settings and mapping
   self.settings = ENAnalyzer::analyzerSettings
-
-
-  def self.genMapping(settings)
-    fieldhash = Hash.new
-    @field_info.each do |f|
-      # Set mapping
-      map = Hash.new
-      if f["Mapping"] == "not_analyzed"
-        map[:index] = f["Mapping"]
-      elsif f["Mapping"] == "english"
-        map[:analyzer] = "custom_en_analyzer"
-      end
-    
-      # If facet, make separate analyzed version. 
-      if @facet_fields.include?(f["Field Name"])
-        attribute (f["Field Name"]+"_analyzed").to_sym, f["Type"], mapping: map
-        attribute f["Field Name"].to_sym, f["Type"], mapping: {index: "not_analyzed", fielddata: {format: "doc_values"}}
-      else # For non-facets                    
-        attribute f["Field Name"].to_sym, f["Type"], mapping: map
-      end
-    end
-  end
-
   genMapping(settings)
 end
---
+++ app/models/model_gen.rb
@@ -0,0 +1,42 @@
+module ModelGen
+  # Generate mapping based on dataspec/settings
+  def genMapping(settings)
+    fieldhash = Hash.new
+    @field_info.each do |f|
+      # Set mapping
+      map = fieldMapping(f)
+
+      # If facet, make separate analyzed version.
+      if @facet_fields.include?(f["Field Name"])
+        addToMapping(f, f["Field Name"]+"_analyzed", map)
+        duplicateUnanalyzedFacet(f)
+      else # For non-facets
+        addToMapping(f, f["Field Name"], map)
+      end
+    end
+  end
+
+  # Add non-facet field to mapping (name, data type, mapping)
+  def addToMapping(f, field_name, map)
+    return attribute field_name.to_sym, f["Type"], mapping: map
+  end
+
+  # Make a duplicate that isn't analyzed for facets
+  def duplicateUnanalyzedFacet(f)
+    return attribute f["Field Name"].to_sym, f["Type"], mapping: {index: "not_analyzed", fielddata: {format: "doc_values"}}
+  end
+
+  # Generate mapping for particular field
+  def fieldMapping(f)
+    map = Hash.new
+
+    # Set to correct analyzer
+    if f["Mapping"] == "not_analyzed"
+      map[:index] = f["Mapping"]
+    elsif f["Mapping"] == "english"
+      map[:analyzer] = "custom_en_analyzer"
+    end
+
+    return map
+  end
+end
