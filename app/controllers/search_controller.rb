25227aeb1a62e8556cc976b51252b607292da90e
shidash <shidash@shidash.com>
2016-02-03 06:02:37 -0500

    Fixed error on search with no params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,18 +7,23 @@ class SearchController < ApplicationController
     # Calculate start and page num
     pagenum, start = page_calc(params)
     
-    # Pass params to SearchQuery model (which builds query and gets results)
-    params.delete_if { |k, v| v.empty? }
-    s = SearchQuery.new(params, start)
-    query, model_to_search = s.build_query
+    # Only if there are params run
+    if !params.except("controller").except("action").empty?
+      # Pass params to SearchQuery model (which builds query and gets results)
+      params.delete_if { |k, v| v.empty? }
+      s = SearchQuery.new(params, start)
+      query, model_to_search = s.build_query
     
-    # Searches the specified models with query input
-    @docs = Elasticsearch::Model.search(query, model_to_search) 
-    @facets = @docs.response["facets"]
+      # Searches the specified models with query input
+      @docs = Elasticsearch::Model.search(query, model_to_search) 
+      @facets = @docs.response["facets"]
     
-    @pagination = WillPaginate::Collection.create(pagenum, 30, @docs.response.hits.total) do |pager|
-      pager.replace @docs
+      @pagination = WillPaginate::Collection.create(pagenum, 30, @docs.response.hits.total) do |pager|
+        pager.replace @docs
+      end
+      @docs = @docs.response["hits"]["hits"]
+    else
+      redirect_to root_path
     end
-    @docs = @docs.response["hits"]["hits"]
   end
 end
