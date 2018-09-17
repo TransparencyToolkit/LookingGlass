6fdf34d1d09e868d3ba8e780903ff577b385a7c3
Brennan Novak <bnvk@transparencytoolkit.org>
2018-09-17 09:38:14 -0400

    updated name in Doc saver call

--- app/controllers/concerns/save_edited_fields.rb
+++ app/controllers/concerns/save_edited_fields.rb
@@ -1,12 +1,12 @@
 # Saves the editable fields on DocManager
 module SaveEditedFields
-  
+
   # Saves the changed fields for the document
   def save_changed_fields(edited_fields)
     # Get the original document details and replace with new
     original_doc, doc_type = get_document_details(edited_fields)
     changed_doc = amend_document_hash(edited_fields, original_doc)
-    
+
     # Save the edited document
     save_data(ENV['PROJECT_INDEX'], doc_type, [changed_doc])
   end
@@ -18,12 +18,12 @@ module SaveEditedFields
     doc_type = get_dataspec_for_doc(original_doc)["class_name"]
     return original_doc, doc_type
   end
-  
+
   # Change the document hash
   def amend_document_hash(edited_fields, original_doc)
     # Get changed fields and make a new version with them
     changed_fields = edited_fields["items"].to_h.to_a.select{|i| i[1]["edited"] == "true"}.map do |d|
-      [d[0], d[1]["changed_content"]]
+      [d[0], d[1]["changed"]]
     end.to_h
     return original_doc["_source"].merge(changed_fields)
   end
