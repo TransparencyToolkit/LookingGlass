7dca66472505e71b8899499453b01913533bed09
M C McGrath <shidash@shidash.com>
2018-10-15 18:46:10 -0400

    Added count of pre-filtered catalyst docs

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,17 +8,25 @@ class ApiController < ApplicationController
     render :json => list_annotators
   end
 
+  # Get the count of documents matching a recipe
   def recipe_search
-    render :json => { status: "success", message: "Some stuff here" }
+    # Parse the recipe
+    recipe = parse_recipe(params["recipe"])
+    docs_to_process = recipe[:docs_to_process]
+
+    # Get the count
+    count = get_filtered_count(recipe, docs_to_process)
+    render :json => { status: "success", message: "#{count.to_s} documents found matching query. The below text miners will only run on these #{count.to_s} documents." }
   end
 
   # Creates the recipe and annotators and runs the job
   def create_job
-    job = JSON.parse(params["job"])
+    recipe = JSON.parse(params["job"])["recipe"]
+    annotators = JSON.parse(params["job"])["annotators"]
     recipe_id = prepare_recipe(job)
-
+    
     # Create the annotators
-    job["annotators"].each do |annotator|
+    annotators.each do |annotator|
       prepare_annotator(annotator, recipe_id)
     end
     
@@ -34,21 +42,43 @@ class ApiController < ApplicationController
 
   private
 
+  # Get the number of documents matching Catalyst filter query
+  def get_filtered_count(recipe, docs_to_process)
+    # Get the count of the filtered documents
+    if docs_to_process[:run_over] == "within_range"
+      return get_within_range_count(recipe[:index_name], docs_to_process[:field_to_search],
+                                    docs_to_process[:filter_query], docs_to_process[:end_filter_range], recipe[:doc_type])
+    elsif docs_to_process[:run_over] == "matching_query"
+      return get_matching_docs_count(recipe[:index_name], docs_to_process[:field_to_search],
+                                     docs_to_process[:filter_query], recipe[:doc_type])
+    elsif docs_to_process[:run_over] == "all"
+      return get_total_doc_count_for_type(recipe[:index_name], recipe[:doc_type])
+    end
+    
+  end
+
   # Create and save the recipe and return the ID
   def prepare_recipe(job)
     # Reformat the recipe as needed
-    recipe = {
-      title: recipe_name = job["recipe"]["filter_name"],
-      doc_type: job["recipe"]["default_dataspec"],
+    recipe = parse_recipe(job)
+
+    # Create the recipe
+    return create_recipe(JSON.pretty_generate(recipe))
+  end
+
+  # Parse the recipe but don't create
+  def parse_recipe(job)
+    return {
+      title: recipe_name = job["filter_name"],
+      doc_type: job["default_dataspec"],
       index_name: ENV['PROJECT_INDEX'],
       docs_to_process: {
-        run_over: job["recipe"]["run_over"],
-        field_to_search: job["recipe"]["field_to_search"],
-        filter_query: job["recipe"]["filter_query"],
-        end_filter_range: job["recipe"]["end_filter_range"]
+        run_over: job["run_over"],
+        field_to_search: job["field_to_search"],
+        filter_query: job["filter_query"],
+        end_filter_range: job["end_filter_range"]
       }
     }
-    return create_recipe(JSON.pretty_generate(recipe))
   end
 
   # Prepare and save annotator
