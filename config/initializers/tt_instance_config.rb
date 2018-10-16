8f4166305d03c429d095dc853f02cba9b8629d0c
M C McGrath <shidash@shidash.com>
2018-10-16 18:20:30 -0400

    Added link back to document upload form on topbar

--- config/initializers/tt_instance_config.rb
+++ config/initializers/tt_instance_config.rb
@@ -1,6 +1,7 @@
 # For pipeline
 ENV['CATALYST_URL'] = "http://localhost:9004" if ENV['CATALYST_URL'] == nil
 ENV['DOCMANAGER_URL'] = "http://0.0.0.0:3000" if ENV['DOCMANAGER_URL'] == nil
+ENV['DOCUPLOAD_URL'] = "http://localhost:9292" if ENV['DOCUPLOAD_URL'] == nil
 
 # For instance / project
 ENV['PROJECT_INDEX'] = "archive_test" if ENV['PROJECT_INDEX'] == nil
