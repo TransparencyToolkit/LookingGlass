0eed0e4f36f00c66cba71740337d7b14e2025a09
Brennan Novak <bnvk@transparencytoolkit.org>
2019-11-10 15:50:05 -0500

    add rough sending of datetime searches for DateAdded field types

--- public/js/search.js
+++ public/js/search.js
@@ -1,34 +1,14 @@
 /* search.js */
 
-$(document).ready(function() {
+var renderSearchFields = function(value, type, hr_field_label) {
 
-  // Sidebar Toggles
-  $('.tree-toggler').on('click', function() {
-      if ($(this).hasClass('just-minus')) {
-          $(this).addClass('just-plus').removeClass('just-minus');
-      } else if ($(this).hasClass('just-plus')) {
-          $(this).addClass('just-minus').removeClass('just-plus');
-      } else if ($(this).hasClass('plus')) {
-          $(this).addClass('minus').removeClass('plus');
-      } else {
-          $(this).addClass('plus').removeClass('minus');
-      }
-      $(this).parent().children('ul.tree').toggle(300);
-  });
+    // Hide Forms
+    $('#form-search-all').hide();
+    $('#form-search-string').hide();
+    $('#form-search-date').hide();
+    $('#form-search-datetime').hide();
 
-  // Search Forms
-  $('#ftypes').on('change', function() {
-
-      var chosen = $('#ftypes option:selected');
-      var value = chosen.val();
-      var type = chosen.data('type').toLowerCase();
-      var hr_field_label = chosen.data('labelname');
-
-      // Hide Forms
-      $('#form-search-all').hide();
-      $('#form-search-string').hide();
-      $('#form-search-date').hide();
-      $('#form-search-datetime').hide();
+    console.log('change #ftypes with: ' + type)
 
     // Modify Fields
     if (type === 'date') {
@@ -42,11 +22,16 @@ $(document).ready(function() {
     }
     else if (type === 'datetime') {
         // TODO: make timestamp values convert to readable format & vv
-        $('#search-date-start').attr('name', 'startrange_' + value)
-	    $('#search-date-end').attr('name', 'endrange_' + value)
+        console.log($('#search-datetime-start').val() + ' ' + $('#search-datetime-end').val())
 
+        // moment.unix(1576321936).format("YYYY-MM-DD hh:mm");
+        $('#search-datetime-start').attr('name', 'startrange_' + value)
+	    $('#search-datetime-end').attr('name', 'endrange_' + value)
+
+        console.log(datetimepicker_icons)
         $('[data-behaviour~=datetimepicker]').datetimepicker({
-            format: 'YYYY-MM-DD hh:mm',
+            extraFormats: ['X', 'x'],
+            format: 'X',
             icons: datetimepicker_icons
         })
     }
@@ -74,10 +59,38 @@ $(document).ready(function() {
 
     }
 
+}
+
+$(document).ready(function() {
+
+  // Sidebar Toggles
+  $('.tree-toggler').on('click', function() {
+      if ($(this).hasClass('just-minus')) {
+          $(this).addClass('just-plus').removeClass('just-minus');
+      } else if ($(this).hasClass('just-plus')) {
+          $(this).addClass('just-minus').removeClass('just-plus');
+      } else if ($(this).hasClass('plus')) {
+          $(this).addClass('minus').removeClass('plus');
+      } else {
+          $(this).addClass('plus').removeClass('minus');
+      }
+      $(this).parent().children('ul.tree').toggle(300);
+  });
+
+  // Search Forms
+  $('#ftypes').on('change', function() {
+
+    var chosen = $('#ftypes option:selected')
+    var value = chosen.val()
+    var type = chosen.data('type').toLowerCase()
+    var hr_field_label = chosen.data('labelname')
+
+    renderSearchFields(value, type, hr_field_label)
+
     // Show Form
     $('#form-search-' + type)
       .removeClass('hide')
-      .show();
-  });
+      .show()
+  })
 
 })
